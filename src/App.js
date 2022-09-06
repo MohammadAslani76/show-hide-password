c23699e41acb81305ae366a34826492ab918747e
MohammadAslani76 <mohammadaslani001@gmail.com>
2022-09-06 13:47:17 +0430

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiEye,FiEyeOff} from "react-icons/fi";
+import {useState} from "react";
 
 function App() {
+    const [icon,setIcon] = useState(FiEyeOff);
+    const [type,setType] = useState("password");
+    const handleChange = () => {
+        if (type === "password"){
+            setType("text");
+            setIcon(FiEye)
+        }else{
+            setType("password");
+            setIcon(FiEyeOff);
+        }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex justify-center items-center bg-pink-300">
+      <div className="w-60 rounded-2xl bg-white px-4 py-2 flex justify-between items-center">
+          <input type={type} className="outline-0"/>
+          <span className="text-2xl cursor-pointer" onClick={handleChange}>{icon}</span>
+      </div>
     </div>
   );
 }
